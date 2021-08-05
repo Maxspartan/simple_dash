5511e6a5303221018776ec2c7083ac89bcf781ae
Mondal <atanu.c.mondal@accenture.com>
2021-08-05 22:50:29 +0530

    first commit

---
+++ touch Dockerfile.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Aug  5 20:07:56 2021
+
+@author: atanu.c.mondal
+"""
+
---
+++ touch application.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Aug  5 20:06:50 2021
+
+@author: atanu.c.mondal
+"""
+
