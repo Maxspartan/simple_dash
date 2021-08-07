6c9fddbea0a78ba3ada4d8b2e774520432c08258
Mondal <atanu.c.mondal@accenture.com>
2021-08-07 22:38:08 +0530

    app launch

--- touch Dockerfile.py
+++
@@ -1,7 +0,0 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Thu Aug  5 20:07:56 2021
-
-@author: atanu.c.mondal
-"""
-
--- touch application.py
+++
@@ -1,7 +0,0 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Thu Aug  5 20:06:50 2021
-
-@author: atanu.c.mondal
-"""
-
